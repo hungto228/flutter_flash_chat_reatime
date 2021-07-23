d131b7f784c543afa9650a89eea09d5682dfecba
hungto228@gmail.com <hungto228@gmail.com>
2021-07-23 08:51:45 +0700

    add screen login

---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Container(
+                  child: Image.asset("images/logo.png"),
+                  height: 60.0,
+                ),
+                Text(
+                  "Flatchat_",
+                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(25.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text(
+                    "login",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5,
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(25.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text(
+                    "resgitter",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
