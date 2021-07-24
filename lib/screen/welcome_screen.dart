bf27096e87452a696cbc80614c4a52cabf689be1
hungto228@gmail.com <hungto228@gmail.com>
2021-07-24 11:00:30 +0700

    custom animation hero

--- lib/screen/welcome_screen.dart
+++ lib/screen/welcome_screen.dart
@@ -5,15 +5,49 @@ import 'package:flutter_flash_chat_reatime/screen/resgitransion_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   String id = "welcome_screen";
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    // animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    controller.forward();
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -26,11 +60,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: "logo",
                   child: Container(
                     child: Image.asset("images/logo.png"),
+                    //       height: animation.value * 100,
                     height: 60.0,
                   ),
                 ),
                 Text(
                   "Flatchat_",
+                  //"${controller.value.toInt()}%",
                   style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
                 )
               ],
